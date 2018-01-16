b949b2819b86f83c57d22eb9deafff46f890362c
wannisa <wannisa.sa58@snru.ac.th>
2018-01-16 14:22:46 +0700

    เปลี่ยนจาก world เป็น Earth
    ลูกค้าให้เปลี่ยน

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
-public class Main {
+public class HelloEarth {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Earth!");
         //this project writed with java
     }
 }
