4a65d611311d66ad084eb863be3efc5582f377b2
wannisa <wannisa.sa58@snru.ac.th>
2018-01-16 14:18:52 +0700

    สร้างกิ่ง helloworld10time
    เพื่อแสดง helloworld 10ครั้ง

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    	for(int i =1;i<=10;i++){
+        System.out.println("Hello, World! "+i);
         //this project writed with java
+    	}
     }
 }
